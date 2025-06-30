6a2a7c74b8f70213ae3de152ec872d1aac9d1318
Giouxel Kasaika <uxell@Giouxels-MacBook-Air.fritz.box>
2025-06-30 19:35:09 +0200

    error handling if lose db connection, or cannot find a db table etc

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template, redirect, url_for
 from data_manager import DataManager
+from sqlalchemy.exc import OperationalError
 from models import db, Movie, User
 import os
 
@@ -16,21 +17,34 @@ data_manager = DataManager() # Create an object from DataManager class
 
 @app.route('/')
 def home():
-    users = data_manager.get_users()
-    return render_template('index.html', users=users)
+    try:
+        users = data_manager.get_users()
+        return render_template('index.html', users=users)
+    except (IOError,OperationalError) as e:
+        print("An IOError occurred: ", str(e))
+        error = "Sorry, we cannot load the user list right now."
+        return render_template('index.html', users=[], error=error)
 
 
 @app.route('/users', methods=['POST'])
 def create_user():
-    username = request.form.get('name')
-    data_manager.create_user(username)
-    return redirect(url_for('home'))
+    try:
+        username = request.form.get('name')
+        data_manager.create_user(username)
+        return redirect(url_for('home'))
+    except (IOError, OperationalError) as e:
+        print("An IOError occurred: ", str(e))
+        error = "Sorry, we cannot load the user list right now."
+        return render_template('index.html', users=[], error=error)
 
 @app.route('/users/<int:user_id>/movies', methods=['GET', 'POST'])
 def user_movies(user_id):
     if request.method == 'GET':
-        movies = data_manager.get_movies(user_id)
-        return render_template('movies.html', movies=movies, user_id=user_id)
+        try:
+            movies = data_manager.get_movies(user_id)
+            return render_template('movies.html', movies=movies, user_id=user_id)
+        except IOError as e:
+            print("An IOError occurred: ", str(e))
 
     else:
         movie_title = request.form.get('title')
@@ -44,15 +58,29 @@ def user_movies(user_id):
 
 @app.route('/users/<int:user_id>/movies/<int:movie_id>/update', methods=['POST'])
 def update_movie(user_id, movie_id):
-    new_movie_name = request.form.get('new_title')
-    data_manager.update_movie(movie_id, new_movie_name)
-    return redirect(url_for('user_movies', user_id=user_id))
+    try:
+        new_movie_name = request.form.get('new_title')
+        data_manager.update_movie(movie_id, new_movie_name)
+        return redirect(url_for('user_movies', user_id=user_id))
+    except IOError as e:
+        print("An IOError occurred: ", str(e))
 
 @app.route('/users/<int:user_id>/movies/<int:movie_id>/delete', methods=['POST'])
 def delete_user_movie(user_id, movie_id):
-    data_manager.delete_movie(movie_id)
-    return redirect(url_for('user_movies', user_id=user_id))
+    try:
+        data_manager.delete_movie(movie_id)
+        return redirect(url_for('user_movies', user_id=user_id))
+    except IOError as e:
+        print("An IOError occurred: ", str(e))
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
   """
